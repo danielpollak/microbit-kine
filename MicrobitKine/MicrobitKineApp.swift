108b0bfb2889f3f59502d312d442392e09c43ea7
Daniel Pollak <danpollak23@gmail.com>
2025-12-20 17:47:15 -0500

    Initial Commit

---
+++ MicrobitKine/MicrobitKineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicrobitKineApp.swift
+//  MicrobitKine
+//
+//  Created by Daniel Pollak on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MicrobitKineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
